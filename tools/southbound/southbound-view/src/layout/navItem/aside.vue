4df5a4a784ff4facc9498a2b15dc32b9a8ae1284
yanglong <1432431927@qq.com>
2022-10-14 17:19:04 +0800

    列表接口、布局组件
    列表接口、布局组件
    
    删除重复代码
    
    tslint
    
    tslint

---
+++ tools/southbound/southbound-view/src/layout/navItem/aside.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex-shrink-0 aside-wrapper" :style="{width: width + 'px'}">
+    <el-aside :width="width + 'px'">
+      <Vertical :menuShow="menuList"></Vertical>
+    </el-aside>
+  </div>
+</template>
+<script>
+import { defineComponent,reactive, ref, computed } from 'vue';
+import { useStore } from 'vuex';
+import Vertical from './vertical.vue';
+
+export default defineComponent({
+  name: 'layoutAside',
+  components:{Vertical},
+  setup(){
+    let store = useStore();
+    const menuStore = store.state.routesListModule.dynamicRoutes;
+    const menuList = reactive(menuStore);
+   
+    let isExpand = ref(true);
+    const toggleBtn = () => {
+      isExpand.value = !isExpand.value;
+    };
+    let width = computed(() => {
+      return isExpand.value ? 200 : 100;
+    });
+   
+    return{
+      menuList,
+      width,
+      isExpand,
+      toggleBtn
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+.aside-wrapper {
+  width: 200px;
+  position: relative;
+  .toggle-btn {
+    position: absolute;
+    top: 16px; 
+    right: 10px;
+  }
+}
+.el-aside{
+  background-color: #fff;
+  height: calc(100vh - 60px);
+  position: fixed;
+  box-shadow: 1px 0px 4px 0px rgba($color: #000, $alpha: 0.2) ;
+}
+.flex-shrink-0 {
+  flex-shrink: 0;
+}
+</style>
