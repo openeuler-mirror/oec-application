5200b422bc0a760e52757d1ad15f3a9a9e3ebaaf
张一诺 <zhangyinuo4@h-partners.com>
2023-02-28 04:01:08 +0000

    update tools/auto_creat_lib/add/pr_bat.py.
    Signed-off-by: 张一诺 <zhangyinuo4@h-partners.com>

--- tools/auto_creat_lib/add/pr_bat.py
+++ tools/auto_creat_lib/add/pr_bat.py
@@ -44,7 +44,13 @@ def getAllFilesInPath(path):
     global allFileNum
     curPathDirList = []  # 当前路径下的所有文件夹
     files = os.listdir(path)  # 返回当前路径下的所有文件和文件夹
-    for f in files:
+    
+    for dl in curPathDirList:
+        getAllFilesInPath(path + "/" + dl)  # 递归获取当前目录下的文件夹内的文件
+
+
+def fileRoute(file,allFileNum,curPathDirList):
+    for f in file:
         if os.path.isdir(path + "/" + f):
             if f[0] == ".":
                 pass  # 排除隐藏文件夹
@@ -60,8 +66,6 @@ def getAllFilesInPath(path):
                     allYamldata.append(os.path.abspath(path + "/" + f))
                 elif path.split("/")[1] == "oepkgs-management":
                     Inyaml.append(f)
-    for dl in curPathDirList:
-        getAllFilesInPath(path + "/" + dl)  # 递归获取当前目录下的文件夹内的文件
 
 
 def creat_pr():
