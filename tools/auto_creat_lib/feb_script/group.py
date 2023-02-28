d124382f05d05218fe6a0198f786896218566843
张一诺 <zhangyinuo4@h-partners.com>
2023-02-28 01:58:20 +0000

    update tools/auto_creat_lib/feb_script/group.py.
    Signed-off-by: 张一诺 <zhangyinuo4@h-partners.com>

--- tools/auto_creat_lib/feb_script/group.py
+++ tools/auto_creat_lib/feb_script/group.py
@@ -11,7 +11,7 @@
 # See the Mulan PSL v2 for more details.
 # Author: @zhangyinuo
 # Create: 2023-02-27
-# Desc: Submit oec-hardware job automatically on compass-ci
+# Desc: Read the YAML file of the oepkg-management library and write the file to the Excel file.
 
 import base64
 import sys
@@ -26,6 +26,7 @@ from collections import defaultdict
 from xml.etree.ElementTree import parse
 from txdpy import get_Bletter, get_Sletter
 import xlwt
+import logging
 
 srcOepkgsNum = 0
 allFileNum = 0
@@ -43,7 +44,6 @@ Inyaml = []
 name_list = ['gostyle', 'etcd', 'rubocop', 'hdp', 'csmith', 'http_load', 'serving', 'minio', 'jfuzz', 'junit', 'openstack', 'python-fuzz', 'rabbitmq', 'tomcat', 'uwsgi', 'ffmpeg', 'influxdb', 'boost', 'vmware', 'websphere', 'hpcondor', 'haproxy', 'lvs', 'scanoss', 'quartz', 'netmap', '顺水平台大数据', 'elementui', 'qemu', 'java', 'pylint', 'flake8', 'log4cpp', 'horovod', 'scancode-toolkit', 'vue', 'yum', 'prometheus', 'eureka', ' nmap ', 'docker', 'openstack', 'vmware', 'grpc-java', 'lmbench', 'aavmf', 'leveldb', 'ovirt', 'calico', 'fio', 'flute、libbase', 'gostub', '1823', 'checkstyle', 'gmock', 'tensorboard', 'puppet', 'memcached', 'fcgi', 'wrk', 'alf-fuzz', 'canal', 'iperf', 'varnish', 'chromium', 'libfonts', 'pytorch', 'ceph', 'jetty', 'caffe', 'springcloud', 'zeromq', 'slurm', 'zfs', 'envoy', 'jemalloc', 'mysql', 'keepalived', 'oracle db2', 'pythonchecker', 'spring framework', 'benchmarksql', 'rocksdb', '.net core', 'libserializer', 'springboot', 'xml-commons-apis', 'ats', 'fastdb', 'sac', 'ambari ', 'lustre', 'rapidonvif ', 'containerd', 'redis', 'kafkacat', 'pentaho-reporting-flow-engine', 'greenplum ', 'clickhouse', 'librepository', 'ambari', 'libvirt', 'kvm', 'libreoffice', 'guacamole', 'opencv', 'unittest', 'lib61850', 'mysql', ' exosip', 'clangtidy', 'pentaho-libxml', 'django', 'milvus', 'spring boot', 'kafka', 'gitlab-ci', 'libvirt', 'lstio', 'grafana', 'jboss', 'webbench', 'liblayout', 'apache http server', 'openmpi', 'mongodb', 'libformula', 'greenplum', 'pigz', 'gtest', 'tpccrunner', 'virtual box', ' node midea server', 'rocketmq', 'netperf', 'sentinel', 'libloader', 'lldpd ', 'cassandra', 'rabbitmq', 'tomcat', 'elasticsearch', 'licensecheck', 'fixbotengine-cxx', 'mongoose', 'otter', 'qemu', 'curl', 'openhpc', 'gnu', 'flannel', 'nagios', 'pacemaker', 'nvidia-docker', 'k8s', 'zabbix', 'stratovirt', 'spring cloud', 'telegraf', 'paddlepaddle', 'sysbench', 'tensorflow 2', 'netty', 'tengine', 'sonarqube', 'tesseract', 'tensorflow', 'lzo', 'jmeter', 'postgresql', 'apollo', 'glm-devel', 'coredns', 'siege', 'cdh', 'docker', 'lpsolve-devel', 'sphinx', 'kubernetes', 'node.js', 'arcface', 'caffe', 'open vswitch', '3908/3508', 'nunit', 'glassfish', 'orientdb', 'libreoffice', 'squid', 'nginx', 'websvn', 'secbinarycheck', '3908 raid ', 'fixbotengine-java', 'convey', ' fastdfs', 'echarts', 'nodejs', 'activemq', 'mariadb', 'ansible', 'unixbench', 'javafuzzer']
 
 def getAllFilesInPath(path):
-    # print(path)
     global allFileNum
     curPathDirList = []  # 当前路径下的所有文件夹
     files = os.listdir(path)  # 返回当前路径下的所有文件和文件夹
@@ -59,8 +59,6 @@ def getAllFilesInPath(path):
                 allFileNum = allFileNum + 1
                 # 总文件数+1
             if f[-5:] == ".yaml" and f != "sig-info.yaml":
-                # yaml_name = f[:-5]
-                # allYamlList.append(yaml_name)
                 if path.split("/")[1] == "oepkgs-management_10":
                     allYamldata.append(os.path.abspath(path + "/" + f))
                 elif path.split("/")[1] == "oepkgs-management":
@@ -87,13 +85,8 @@ if __name__ == '__main__':
     a = 0
     # 创建pr
     os.system("git clone 'https://gitee.com/zhang-yn/oepkgs-management.git';")
-    print("--------")
     getAllFilesInPath("./oepkgs-management/sig")
-    print("********")
-    print(Inyaml)
-    print("===========")
     print(len(Inyaml))
-    print("---------------")
     wb = xlwt.Workbook()
     ws = wb.add_sheet('1 sheet')
     line = 0
@@ -102,7 +95,6 @@ if __name__ == '__main__':
     for i in Inyaml:
         read_yaml(i)
         line = line + 1
-    print("------ test -----")
+    logging.info("------ test -----")
     wb.save('1.xls')
-    print("=========")
-    print(else_list)
+    print("==== end =====")
