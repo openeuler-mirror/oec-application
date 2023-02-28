78c3acb38b122ca0dbcce90e9cc92cdbb76ced1b
张一诺 <zhangyinuo4@h-partners.com>
2023-02-28 04:02:41 +0000

    update tools/auto_creat_lib/add/upload_rpmcode.py.
    Signed-off-by: 张一诺 <zhangyinuo4@h-partners.com>

--- tools/auto_creat_lib/add/upload_rpmcode.py
+++ tools/auto_creat_lib/add/upload_rpmcode.py
@@ -49,6 +49,11 @@ def getAllFilesInPath(path):
     global allFileNum
     curPathDirList = []  # 当前路径下的所有文件夹
     files = os.listdir(path)  # 返回当前路径下的所有文件和文件夹
+    fileRoute(files,allFileNum,curPathDirList)
+    for dl in curPathDirList:
+        getAllFilesInPath(path + "/" + dl)  # 递归获取当前目录下的文件夹内的文件
+
+def fileRoute(file,allFileNum,curPathDirList):
     for f in files:
         if os.path.isdir(path + "/" + f):
             if f[0] == ".":
@@ -63,10 +68,6 @@ def getAllFilesInPath(path):
             if f[-5:] == ".yaml" and f != "sig-info.yaml":
                 yaml_name = f[:-5]
                 allYamlList.append(yaml_name)
-                # allYamldata.append(os.path.abspath(path + "/" + f))
-
-    for dl in curPathDirList:
-        getAllFilesInPath(path + "/" + dl)  # 递归获取当前目录下的文件夹内的文件
 
 
 # 获取rpm信息，拿到name和description
